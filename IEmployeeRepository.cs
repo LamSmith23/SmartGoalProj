e4800a19b41723fdc5ed4594bc0a4ec83e81af05
LamSmith23 <lsmith@techtonicgroup.com>
2017-12-11 15:19:11 -0700

    Add files via upload
    Somehow my original DataAccessLayer repository was deleted. Recreated Repository for my DataAccess Class

---
+++ IEmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace DataAccess.Employees
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAllEmployees(); 
+        Employee GetDetailsByID(int id);
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(int id);
+        void UpdateEmployee(Employee employee);
+        void Save();
+        void Dispose();
+    }
+}
